9580fd86f964eba41c8257610d71b26e89e6df36
houlijia <wohoulijia@163.com>
2024-05-14 00:35:09 +0800

    update project

--- thread/threading.cpp
+++ thread/threading.cpp
@@ -5,21 +5,23 @@
 **/
 #include "threading.h"
 #include <mutex>
+#include <atomic>
 
 mutex mtx1;  //创建互斥量
 mutex mtx2;  //创建互斥量
-int globalvariables = 0;
+//int globalvariables = 0;
+atomic<int> globalvariables = 0;   //原子
 
 void task1(){
     for (int i = 0; i < 10000; ++i) {
         // RAII  资源获取及初始化
         //lock_guard<mutex> lock(mtx);   //可以避免  throw return场景
         //mtx.lock();   //线程不安全的地方枷锁、同unlock()一起
-        lock(mtx1,mtx2);   //给多个互斥量上锁
+//        lock(mtx1,mtx2);   //给多个互斥量上锁
         globalvariables++;
         globalvariables--;
-        mtx1.unlock();
-        mtx2.unlock();
+//        mtx1.unlock();
+//        mtx2.unlock();
         //callfun()  //throw
 //        if(1==1){
 //            return ;
@@ -33,12 +35,12 @@ void task2(){
         //lock_guard<mutex> lock(mtx);   //可以避免  throw return场景、lock_guard作用域在整个for循环、自动结果
         //mtx.lock();   //线程不安全的地方枷锁、同unlock()一起
         //lock(mtx1,mtx2);   //给多个互斥量上锁
-        unique_lock<mutex> lock3(mtx1);   //unique_lock锁比较灵活，可以随时解锁
-        lock3.unlock();
+//        unique_lock<mutex> lock3(mtx1);   //unique_lock锁比较灵活，可以随时解锁
+//        lock3.unlock();
         globalvariables++;
         globalvariables--;
-        mtx1.unlock();
-        mtx2.unlock();
+//        mtx1.unlock();
+//        mtx2.unlock();
         //callfun()  //throw
 //        if(1==1){
 //            return ;
