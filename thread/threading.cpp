cff34c7517f4687564df3e2cc25250007031a40a
houlijia <wohoulijia@163.com>
2024-05-14 00:30:26 +0800

    update project

--- thread/threading.cpp
+++ thread/threading.cpp
@@ -6,15 +6,39 @@
 #include "threading.h"
 #include <mutex>
 
-mutex mtx;  //创建互斥量
+mutex mtx1;  //创建互斥量
+mutex mtx2;  //创建互斥量
 int globalvariables = 0;
 
 void task1(){
     for (int i = 0; i < 10000; ++i) {
-        lock_guard<mutex> lock(mtx);   //可以避免  throw return场景
+        // RAII  资源获取及初始化
+        //lock_guard<mutex> lock(mtx);   //可以避免  throw return场景
         //mtx.lock();   //线程不安全的地方枷锁、同unlock()一起
+        lock(mtx1,mtx2);   //给多个互斥量上锁
         globalvariables++;
         globalvariables--;
+        mtx1.unlock();
+        mtx2.unlock();
+        //callfun()  //throw
+//        if(1==1){
+//            return ;
+//        }
+        //mtx.unlock();  //解锁
+    }
+}
+void task2(){
+    for (int i = 0; i < 10000; ++i) {
+        // RAII  资源获取及初始化
+        //lock_guard<mutex> lock(mtx);   //可以避免  throw return场景、lock_guard作用域在整个for循环、自动结果
+        //mtx.lock();   //线程不安全的地方枷锁、同unlock()一起
+        //lock(mtx1,mtx2);   //给多个互斥量上锁
+        unique_lock<mutex> lock3(mtx1);   //unique_lock锁比较灵活，可以随时解锁
+        lock3.unlock();
+        globalvariables++;
+        globalvariables--;
+        mtx1.unlock();
+        mtx2.unlock();
         //callfun()  //throw
 //        if(1==1){
 //            return ;
@@ -33,7 +57,7 @@ void testfun(void){
 void funthreading(void){
     //多线程
     thread t1(task1);
-    thread t2(task1);
+    thread t2(task2);
     t1.join();
     t2.join();
     cout<<" current values is : "<<globalvariables<<endl;
