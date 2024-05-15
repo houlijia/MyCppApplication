22a6cf737c7c3cbfb9373b3bebcebaab85bf2a4a
houlijia <wohoulijia@163.com>
2024-05-15 23:39:20 +0800

    update project

--- stl/stlvariables.cpp
+++ stl/stlvariables.cpp
@@ -4,9 +4,29 @@
  * @Desc
 **/
 #include "stlvariables.h"
+struct struct01{
+    float x,y,z;
+    struct01(float x,float y,float z):x(x),y(y),z(z){}   //struct01的构造函数
+    struct01(const struct01& struct01):x(struct01.x),y(struct01.y),z(struct01.z){
+        cout<<"copy 构造函数"<<endl;
+    }
+};
 vector<int> testvector(){
     // 创建一个向量存储 int
     int n=10;
+    vector<struct01> vectstruct;
+    //cout<<"vectstruct 的第二个元素为："<< &*vectstruct.begin()<< endl;
+    vectstruct.reserve(3);
+    vectstruct.emplace_back(1,2,3);
+    vectstruct.emplace_back(4,5,6);
+    vectstruct.emplace_back(7,8,9);   //优化复制   将push_back改成emplace_back
+    //vectstruct.push_back({1,2,3});
+//    vectstruct.push_back({2,3,4});
+//    vectstruct.push_back({3,4,5});
+    cout<<"vectstruct 的第二个元素为："<< &*vectstruct.begin()<< endl;
+    //vectstruct.erase(vectstruct.begin()+1);       //删除第二个值
+    //vectstruct.clear();              //将vectstruct清除
+
     vector<int> vecvar;              // vector动态数组，支持快速随机访问
     vector<int> v01(n);              //定义一个长度为n的数组，动态定义，不指定初值默认初值为0
     vector<double> v02(n, 0);     //所有的元素均为0
@@ -41,7 +61,7 @@ vector<int> testvector(){
     cout<<"清除 vecvar 元素个数后 大小为： "<<vecvar.size()<<endl;
     int n02=3;
     vecvar.resize(n02,12);
-    for (auto i:vecvar) {
+    for (auto& i:vecvar) {    //是将vecvar拷贝至for循环中，加上&，避免拷贝，是引用
         cout<<"改变大小后元素内容: vecvar["<<i<<"]= "<<i<<endl;
     }
     auto address= *(vecvar.begin());
